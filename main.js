118c70fdead590e7c10575a0b750c49205e161f5
yoenheep <yoen3212@naver.com>
2025-02-10 12:03:50 +0900

    제이쿼리 연결 및 add생성

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   console.log("ready");
   $("#addBtn").click(function () {
     let newLi = `<li id="listLi">${$("#addText").val()}</li>`;
-    let delBtn = `<button id="delBtn">-</button>`;
+    let delBtn = `<button id="delBtn">삭제</button>`;
     $("#ulList").append(newLi);
     newLi.append(delBtn);
   });
